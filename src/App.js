629e935104229e71b14cf12b4050dc33525506bb
David Almaraz <david.almaraz@theksquare.com>
2021-03-31 23:22:17 -0600

    Modification1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import logo from './logo.svg';
+import imagen from './Professional Picture 3.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src = {imagen} width="200px" height ="250px"/>
+       <p>Hello, I'm David Almaraz and I am a mechatronic engineer graduated from the Autonomous University of Yucatán, I have knowledge of automation, 
+         electronics and software. Currently I am dedicated to the software development of Javascript in Ksquare University</p>
       </header>
     </div>
   );
